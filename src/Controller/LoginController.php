06668f3598a4e907fac74766330b2a0aa2990acb
FabianJesus <fabian.jesus.jm@gmail.com>
2025-02-12 20:22:57 +0100

    second code update

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -2,60 +2,25 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use App\Service\JwtTokenService;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-
+use Symfony\Component\Routing\Attribute\Route;
+use App\Service\LoginService;
 
 class LoginController extends AbstractController
 {
 
-    private $entity_manager;
-    private $jwtService;
-    private $pass_hasher;
+    private LoginService $loginService;
 
-    public function __construct(EntityManagerInterface $entity_manager,JwtTokenService $jwtService,UserPasswordHasherInterface $pass_hasher)
+    public function __construct(LoginService $loginService)
     {
-        $this->entity_manager = $entity_manager;
-        $this->jwtService = $jwtService;
-        $this->pass_hasher = $pass_hasher;
+        $this->loginService = $loginService;
     }
 
     #[Route('/api/login', methods:"POST")]
-    public function login(Request $request): Response
+    public function login(Request $request)
     {
-        $data = json_decode($request->getContent(), true);
-        $email = $data['email'] ?? '';
-        $password = $data['password'] ?? '';
-
-        $user = $this->entity_manager->getRepository(User::class)->findOneBy(['email' => $email]);
-
-        if (!$user or !$this->pass_hasher->isPasswordValid($user, $password)) {
-            return new JsonResponse(
-                ['request_denied' => 'Unauthorized'],
-                401                         
-            );
-        }
-
-        $claims = [
-            'email' => $email,
-        ];
-        
-        $token = $this->jwtService->generateToken($claims, 3600);
-
-        $tokenString = $token->toString();
-        return new JsonResponse(['token' => $token->toString()]);
-
-        return new JsonResponse(
-            ['request_denied' => 'Unauthorized'],
-            401                         
-        );
+        return $this->loginService->loginCheck($request);
     }
 
 }
---
+++ src/Service/LoginService.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\UserRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use App\Service\JwtTokenService;
+
+class LoginService
+{
+
+    private JwtTokenService $jwtService;
+    private UserPasswordHasherInterface $passHasher;
+    private UserRepository $usuarioReposity;
+
+    public function __construct(UserRepository $usuarioReposity,JwtTokenService $jwtService,UserPasswordHasherInterface $passHasher)
+    {
+        $this->jwtService = $jwtService;
+        $this->passHasher = $passHasher;
+        $this->usuarioReposity = $usuarioReposity;
+    }
+
+    public function loginCheck($request)
+    {
+        $data = json_decode($request->getContent(), true);
+        $email = $data['email'] ?? '';
+        $password = $data['password'] ?? '';
+        $user = $this->usuarioReposity->findOneBy(['email' => $email]);
+        if (!$user or !$this->passHasher->isPasswordValid($user, $password)) {
+            return new JsonResponse(
+                ['request_denied' => 'Unauthorized'],
+                401                         
+            );
+        }
+
+        $claims = [
+            'email' => $email,
+        ];
+        
+        $token = $this->jwtService->generateToken($claims, 3600);
+        return new JsonResponse(['token' => $token->toString()]);
+    }
+
+}
